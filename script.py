4f3dbba3bc516c122b83201a48bfeae8c4dd44f4
LH52 <louayhelou52@gmail.com>
2026-05-01 12:13:34 -0400

    all done

---
+++ script.py
@@ -0,0 +1,39 @@
+import pyautogui
+import pyperclip
+import tkinter as tk
+import keyboard
+
+is_playing = False
+
+def press_tab():
+    
+    pyautogui.keyUp('ctrl')
+    pyautogui.keyUp('shift')
+    pyautogui.keyUp('v')
+    
+    text = pyperclip.paste()
+    values = text.split('#separator#')
+    for i in range(0, len(values)):
+        
+        pyautogui.write(values[i])  # interval prevents skipping
+        if i < len(values) - 1:
+            pyautogui.press('tab')
+
+
+def toggle_play_pause():
+    global is_playing
+    is_playing = not is_playing
+    if is_playing:
+        btn.config(text="Pause")
+        keyboard.add_hotkey('ctrl+shift+p', press_tab)
+    else:
+        btn.config(text="Play")
+        keyboard.remove_hotkey('ctrl+shift+p')
+
+window = tk.Tk()
+window.title("GP Paster")
+
+btn = tk.Button(window, text="Play", command=toggle_play_pause)
+btn.pack(pady=10)
+
+window.mainloop()
